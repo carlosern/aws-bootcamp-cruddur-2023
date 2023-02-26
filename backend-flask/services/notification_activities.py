c88383d2321a76407a560c18ea88e3486999ff3d
Carlos Rojas <carlosern@gmail.com>
2023-02-26 02:14:45 +0000

    Introducing some random delay for the notification service

--- backend-flask/services/notification_activities.py
+++ backend-flask/services/notification_activities.py
@@ -1,6 +1,11 @@
+import time
+import random
 from datetime import datetime, timedelta, timezone
 class NotificationActivities:
   def run():
+    #just for test the latency
+    time.sleep(random.randint(1, 5)) # Sleep for 3 seconds
+
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
