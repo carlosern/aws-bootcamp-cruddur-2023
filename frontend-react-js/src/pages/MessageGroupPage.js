d5d7c52a2ee40f187f4d4891583904c0a7488744
Carlos Rojas <carlosern@gmail.com>
2023-03-12 20:30:56 +0000

    Pass in Authorization header en get methods

--- frontend-react-js/src/pages/MessageGroupPage.js
+++ frontend-react-js/src/pages/MessageGroupPage.js
@@ -23,6 +23,9 @@ export default function MessageGroupPage() {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/message_groups`
       const res = await fetch(backend_url, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+        },           
         method: "GET"
       });
       let resJson = await res.json();
@@ -41,6 +44,9 @@ export default function MessageGroupPage() {
       const handle = `@${params.handle}`;
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/messages/${handle}`
       const res = await fetch(backend_url, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+        },           
         method: "GET"
       });
       let resJson = await res.json();
