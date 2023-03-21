9d767f8fd02e04277e6b4d52fe9b5e4aa8bd9089
Carlos Rojas <carlosern@gmail.com>
2023-03-21 02:26:05 +0000

    post confirmation lambda is working!

--- aws/lambdas/cruddur-post-confirmation.py
+++ aws/lambdas/cruddur-post-confirmation.py
@@ -3,22 +3,25 @@ import psycopg2
 import os
 
 global conn
-
 def lambda_handler(event, context):
+    print(event)
     user = event['request']['userAttributes']
     try:
-        # conn = psycopg2.connect(
-        #     host=(os.getenv('PG_HOSTNAME')),
-        #     database=(os.getenv('PG_DATABASE')),
-        #     user=(os.getenv('PG_USERNAME')),
-        #     password=(os.getenv('PG_SECRET'))
-        # )
-
         conn = psycopg2.connect(
-             host=(os.getenv('CONNECTION_URL'))
+            host=(os.getenv('PG_HOSTNAME')),
+            database=(os.getenv('PG_DATABASE')),
+            user=(os.getenv('PG_USERNAME')),
+            password=(os.getenv('PG_SECRET'))
         )
+
+        # Connect to an existing database
+        #with psycopg2.connect(os.getenv('CONNECTION_URL')) as conn:
+    
+        # conn = psycopg2.connect(
+        #      host=(os.getenv('CONNECTION_URL'))
+        # )
         cur = conn.cursor()
-        cur.execute("INSERT INTO users (display_name, handle, cognito_user_id, email) VALUES(%s, %s, %s, %s)", (user['name'], user['email'], user['sub'], user['email']))
+        cur.execute("INSERT INTO users (display_name, handle, cognito_user_id, email) VALUES(%s, %s, %s, %s)", (user['name'], user['preferred_username'], user['sub'], user['email']))
         conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
