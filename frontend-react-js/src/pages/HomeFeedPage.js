5b92301d4966fdeb59ecddd2c60918bae2cbcfd6
Carlos Rojas <carlosern@gmail.com>
2023-04-19 22:04:23 +0000

    refactoring CheckAuth function

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -10,8 +10,7 @@ import ReplyForm from '../components/ReplyForm';
 // [TODO] Authenication
 //import Cookies from 'js-cookie'
 
-import { Auth } from 'aws-amplify';
-
+import CheckAuth from '../lib/CheckAuth'
 
 export default function HomeFeedPage() {
   const [activities, setActivities] = React.useState([]);
@@ -52,25 +51,7 @@ export default function HomeFeedPage() {
   //   }
   // };
 
-  // check if we are authenicated
-const checkAuth = async () => {
-  Auth.currentAuthenticatedUser({
-    // Optional, By default is false. 
-    // If set to true, this call will send a 
-    // request to Cognito to get the latest user data
-    bypassCache: false 
-  })
-  .then((user) => {
-    console.log('user',user);
-    return Auth.currentAuthenticatedUser()
-  }).then((cognito_user) => {
-      setUser({
-        display_name: cognito_user.attributes.name,
-        handle: cognito_user.attributes.preferred_username
-      })
-  })
-  .catch((err) => console.log(err));
-};
+
 
   React.useEffect(()=>{
     //prevents double call
@@ -78,7 +59,7 @@ const checkAuth = async () => {
     dataFetchedRef.current = true;
 
     loadData();
-    checkAuth();
+    CheckAuth(setUser);
   }, [])
 
   return (
