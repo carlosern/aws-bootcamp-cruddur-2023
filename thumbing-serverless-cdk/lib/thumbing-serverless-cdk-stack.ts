be22f143c495fc38a3a0a416343fea26f7f36b6e
Carlos Rojas <carlosern@gmail.com>
2023-06-27 02:58:12 +0000

    Week 8 - process-images function

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -1,7 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 
 import { Construct } from 'constructs';
+
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export class ThumbingServerlessCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -10,7 +15,12 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
     // The code that defines your stack goes here
 
     const bucketName: string= process.env.THUMBING_BUCKET_NAME as string;
+    const functionPath: string = process.env.THUMBING_FUNCTION_PATH as string;
+    const folderInput: string = process.env.THUMBING_S3_FOLDER_INPUT as string;
+    const folderOutput: string = process.env.THUMBING_S3_FOLDER_OUTPUT as string;
+    
     const bucket = this.createBucket(bucketName);
+    const lambda = this.createLambda(functionPath, bucketName, folderInput, folderOutput);
 
 
   }
@@ -24,6 +34,27 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
     return bucket;
   }
 
+  //lambdas retornar IFuntions
+  createLambda(
+    functionPath: string, 
+    bucketName: string,
+    folderInput: string,
+    folderOutput: string) : lambda.IFunction{
+    const lambdaFunction = new lambda.Function(this, 'ThumbLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(functionPath),
+      environment:{
+        DEST_BUCKET_NAME: bucketName,
+        FOLDER_INPUT: folderInput,
+        FOLDER_OUTPUT: folderOutput,
+        PROCESS_WITDTH: '512',
+        PROCESS_HEIGTH: '512'
+      }
+
+    })
+    return lambdaFunction;
+  }
 
   
 }
