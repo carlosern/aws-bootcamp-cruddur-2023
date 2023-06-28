02032e86505aa85f6ede1056ceb2e12fbc25c6dc
Carlos Rojas <carlosern@gmail.com>
2023-06-28 13:26:06 +0000

    week 8 - wip Implement CDK Stack

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications'
 
 import { Construct } from 'constructs';
 
@@ -15,12 +16,24 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
     // The code that defines your stack goes here
 
     const bucketName: string= process.env.THUMBING_BUCKET_NAME as string;
+    const assetsBucketName: string = process.env.ASSETS_BUCKET_NAME as string;
     const functionPath: string = process.env.THUMBING_FUNCTION_PATH as string;
     const folderInput: string = process.env.THUMBING_S3_FOLDER_INPUT as string;
     const folderOutput: string = process.env.THUMBING_S3_FOLDER_OUTPUT as string;
     
+    console.log('uploadsBucketName',bucketName)
+    console.log('assetsBucketName',assetsBucketName)
+    console.log('folderInput',folderInput)
+    console.log('folderOutput',folderOutput)
+    // console.log('webhookUrl',webhookUrl)
+    // console.log('topicName',topicName)
+    console.log('functionPath',functionPath)
+
     const bucket = this.createBucket(bucketName);
+    const assetsBucket = this.importBucket(assetsBucketName);
     const lambda = this.createLambda(functionPath, bucketName, folderInput, folderOutput);
+
+    this.createS3NotifyToLambda(folderInput, lambda, bucket);
 
 
   }
@@ -31,6 +44,11 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
       bucketName: bucketName,
       removalPolicy: cdk.RemovalPolicy.DESTROY
     } );
+    return bucket;
+  }
+
+  importBucket(bucketName: string): s3.IBucket {
+    const bucket = s3.Bucket.fromBucketName(this,"AssetsBucket",bucketName);
     return bucket;
   }
 
@@ -56,6 +74,14 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
     return lambdaFunction;
   }
 
-  
+  createS3NotifyToLambda(prefix: string, lambda: lambda.IFunction, bucket: s3.IBucket): void {
+    const destination = new s3n.LambdaDestination(lambda);
+    bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED_PUT,
+      destination,
+      {prefix: prefix} // folder to contain the original images
+    )
+  }
+
 }
 
